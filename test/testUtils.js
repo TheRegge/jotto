8a794741d86996c9ba671afbda443cc013d49b1c
Regis Zaleman <regis.zaleman@gmail.com>
2019-05-29 16:20:03 -0400

    Start test for GuessedWords

--- test/testUtils.js
+++ test/testUtils.js
@@ -2,12 +2,12 @@ import checkPropTypes from 'check-prop-types'
 
 /**
  * Return node(s) with the given data-test attribute.
- * @param {ShallowWrapper} wrappper - Enzyme shallow wrapper.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
  * @param {string} val - Value of data-test attribute for search.
  * @returns {ShallowWrapper}
  */
-export const findByTestAttr = (wrappper, val) => {
-    return wrappper.find(`[data-test="${val}"]`)
+export const findByTestAttr = (wrapper, val) => {
+    return wrapper.find(`[data-test="${val}"]`)
 }
 
 export const checkProps = (component, conformingProps) => {
