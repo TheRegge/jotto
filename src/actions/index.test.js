56c31fdbce6524091096936d92837dcfd263b1de
Regis Zaleman <regis.zaleman@gmail.com>
2019-05-30 15:51:16 -0400

    Add secretWord action creator and reducer

---
+++ src/actions/index.test.js
@@ -0,0 +1,33 @@
+import moxios from 'moxios'
+
+import { storeFactory } from '../../test/testUtils'
+import { getSecretWord } from './'
+
+describe('getSecretWord action creator', () => {
+    beforeEach(() => {
+        moxios.install()
+    })
+    afterEach(() => {
+        moxios.uninstall()
+    })
+    test('add response word to state', () => {
+        const secretWord = 'party'
+        const store = storeFactory()
+
+        moxios.wait(() => {
+            const request = moxios.requests.mostRecent()
+            request.respondWith({
+                status: 200,
+                response: secretWord,
+            })
+        })
+
+        // We 'return' the promise so the test
+        // will only assess when all async is done
+        return store.dispatch(getSecretWord())
+        .then(() => {
+            const newState = store.getState()
+            expect(newState.secretWord).toBe(secretWord)
+        })
+    })
+})
