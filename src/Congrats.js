34dd6e88ed7630bd0af943298a1be953c58f0933
Regis Zaleman <regis.zaleman@gmail.com>
2019-05-31 15:45:16 -0400

    Refactor `Congrats` component to hide empty alert

--- src/Congrats.js
+++ src/Congrats.js
@@ -7,15 +7,21 @@ import PropTypes from 'prop-types'
  * @param {object} props - React props.
  * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
  */
-const Congrats = (props) => (
-    <div data-test="component-congrats" className="alert alert-success">
-        {
-            props.success && (<span data-test="congrats-message">
-                Congratulations! You guess the word!
-            </span>)
-        }
-    </div>
-)
+const Congrats = (props) => {
+    if (props.success) {
+      return (
+        <div data-test="component-congrats" className="alert alert-success">
+          <span data-test="congrats-message">
+            Congratulations! You guessed the word!
+          </span>
+        </div>
+      )
+    } else {
+      return (
+        <div data-test="component-congrats" />
+      )
+    }
+  }
 
 Congrats.propTypes = {
     success: PropTypes.bool.isRequired,
