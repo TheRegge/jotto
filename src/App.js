4249e9d8e8bbbdd11967dda9f5b28586a7620fed
Regis Zaleman <regis.zaleman@gmail.com>
2019-05-31 10:42:48 -0400

    Add getSecretWord action creator on App mount

--- src/App.js
+++ src/App.js
@@ -7,12 +7,24 @@ import Congrats from './Congrats'
 import Input from './Input'
 import './App.css';
 
-class App extends Component {
+export class UnconnectedApp extends Component {
+
+  /**
+   * @method componentDidMount
+   * @memberof App
+   * @returns {undefined}
+   */
+  componentDidMount () {
+    // get the secret word
+    this.props.getSecretWord()
+  }
+
   render() {
     return (
       <div className="container">
         <h1>Jotto</h1>
         <Congrats success={this.props.success} />
+        {this.props.secretWord}
         <Input />
         <GuessedWords guessedWords={this.props.guessedWords} />
       </div>
@@ -24,4 +36,4 @@ const mapStateToProps = ({ success, secretWord, guessedWords }) => {
   return { success, secretWord, guessedWords }
 }
 
-export default connect(mapStateToProps, { getSecretWord })(App);
+export default connect(mapStateToProps, { getSecretWord })(UnconnectedApp);
--- src/reducers/secretWordReducer.js
+++ src/reducers/secretWordReducer.js
@@ -13,5 +13,4 @@ export default (state=null, action) => {
         default: 
             return state
     }
-    return state
 }
